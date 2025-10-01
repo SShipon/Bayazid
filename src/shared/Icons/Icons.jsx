7b467721ac20a994ab9569bba92d9912bfb4bb38
oxshipon1@gmail.com <oxshipon1@gmail.com>
2025-10-02 00:34:10 +0600

    addd link

--- src/shared/Icons/Icons.jsx
+++ src/shared/Icons/Icons.jsx
@@ -3,7 +3,7 @@
 const Icons = () => {
     return (
         <div className='flex gap-5 my-8 text-4xl'>
-                <a href=""><i className="fa-brands fa-facebook "></i></a>
+                <a href="https://www.facebook.com/bayazid.mahmud.900"><i className="fa-brands fa-facebook "></i></a>
                 <a href=""><i className="fa-brands fa-linkedin "></i></a>
                 <a href=""><i className="fa-brands fa-github "></i></a>
                 <a href=""><i className="fa-brands fa-square-instagram "></i></a>
